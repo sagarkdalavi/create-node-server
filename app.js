a1a103ed2392600547f86a138d6b7628b367d147
sagar <sagarkdalavi@gmail.com>
2020-01-05 14:24:46 +0530

    Serving JSON Data

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var http= require('http');
 
 var server = http.createServer(function(req,res){
     console.log("Request was made: "+req.url)
-    res.writeHead(200,{'Content-Type':'text-plain'})
-    res.end('Hey client')
+    res.writeHead(200,{'Content-Type':'application/json'})
+    var myObj ={
+        name: "Sagar",
+        location: "Pune",
+        designation: "Software Engineer"
+    }
+    res.end(JSON.stringify(myObj))
 })
 
-server.listen(3000,'127.0.0.1');
-console.log('Server here, now listening to port 3000');
+server.listen(3001,'127.0.0.1');
+console.log('Server here, now listening to port 3001');
